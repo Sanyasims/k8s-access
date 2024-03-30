a45bcfb29ff26f6460235da1691ae6cc4e1aa809
Nikita <artesan538@gmail.com>
2024-03-30 23:14:09 +0700

    prevent_destroy

--- main.tf
+++ main.tf
@@ -1,10 +1,37 @@
 provider "kubernetes" {
 }
 
+variable "cluster_admins" {
+  type = list(object({
+    name = string
+  }))
+}
+variable "namespaces" {
+  type = list(object({
+    namespace = string,
+    users = list(object({
+      name = string,
+      role = string
+    }))
+  }))
+  default = [ 
+  {
+    namespace = "terraform-example-namespace"
+    users = [
+      {
+        name = "artemenko.n.argonlabs@gmail.com"
+      }
+    ]
+  }
+  ]
 
+}
 
 resource "kubernetes_namespace" "namespace" {
   metadata {
     name = "terraform-example-namespace"
+  }
+  lifecycle {
+    prevent_destroy = true
   }
 }

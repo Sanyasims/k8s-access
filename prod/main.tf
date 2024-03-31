e7ee5168678d0d5a765fb4eb66a8d0064e739737
Nikita <artesan538@gmail.com>
2024-03-31 17:02:42 +0700

    fix

--- prod/main.tf
+++ prod/main.tf
@@ -16,3 +16,22 @@ module "rbac" {
 
   namespaces = var.namespaces
 }
+
+variable "cluster_admins" {
+  type = list(string)
+  default = []
+}
+
+variable "cluster_developers" {
+  type = list(string)
+  default = []
+}
+
+variable "namespaces" {
+  type = list(object({
+    namespace = string,
+    viewers = list(string),
+    editors = list(string)
+  }))
+  default = []
+}
